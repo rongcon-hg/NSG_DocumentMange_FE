b5e27f6e100148f2f5c719ff96a6ede3e476abff
Nguyễn Văn Luyến <nguyenluyen@nsg.edu.vn>
2026-09-11 16:19:27 +0700

    fix(timezone): configure global dayjs default timezone Asia/Ho_Chi_Minh in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,16 @@ import App from './App.jsx';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
+import 'dayjs/locale/vi';
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs.locale('vi');
+dayjs.tz.setDefault('Asia/Ho_Chi_Minh');
+
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <App />
